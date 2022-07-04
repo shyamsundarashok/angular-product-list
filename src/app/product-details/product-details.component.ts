e33d683ea27fe725bd9002498843d4a055685db3
shyamsundarashok <ashok.shyamsundar@gmail.com>
2022-07-04 12:25:11 -0500

    Adding routing into Product Details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) {}
 
   ngOnInit(): void {
+    
   }
-
 }
