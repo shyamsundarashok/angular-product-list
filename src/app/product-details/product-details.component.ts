a3e1eb59575b7bce7c761625ab11ffce09797505
shyamsundarashok <ashok.shyamsundar@gmail.com>
2022-07-04 12:35:09 -0500

    ProductDetailsComponent  functionality added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -13,6 +13,12 @@ export class ProductDetailsComponent implements OnInit {
   constructor(private route:ActivatedRoute) {}
 
   ngOnInit(): void {
-    
+    //First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    //Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 }
