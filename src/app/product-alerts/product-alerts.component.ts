b278cf69b0e7005478ca2ce0fb2b22422a15731d
Shyam Sundar Ashok <ashok.shyamsundar@gmail.com>
2022-07-04 12:05:55 -0500

    To make the Notify Me button work, the child component needs to notify and pass the data to the parent component.

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -7,7 +7,9 @@ import { Product } from '../products';
   styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() product!: Product;
+  @Input() product!: Product | undefined;
+
+  @Output() notify = new EventEmitter();
 
   constructor() {}
 
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,10 +8,14 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  products = [...products];
 
   share() {
     window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
